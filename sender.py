a2054204dfd587695bd89b0e2a7bce973766d63d
Berko Pasha <berk579.32@gmail.com>
2024-03-10 23:28:34 +0300

    trying 1

--- sender.py
+++ sender.py
@@ -1,37 +1,46 @@
 from email.message import EmailMessage
-from passs import password
 import ssl
 import smtplib
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUiType
 import sys
 
-from PyQt5.uic import loadUiType
+ui, _ = loadUiType('senderDesktop.ui')
 
-ui = loadUiType('senderDesktop.ui')
+class MainApp(QMainWindow, ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.Send_Email)
 
-email_sender = 'berk579.32@gmail.com'
-email_password = password
+    def Login_Account(self):
+        email_sender = self.lineEdit.text()
+        email_password = self.lineEdit_2.text()
+        subject = self.lineEdit_5.text()
+        body = self.lineEdit_3.text()
+        email_receiver = self.lineEdit_4.text()
+        em = EmailMessage()  # this is an instance of EmailMessage
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['subject'] = subject
+        em.set_content(body)
 
-email_receiver = ''
+        context = ssl.create_default_context()
 
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
 
-subject = 'staj'
-body = """
+    def Send_Email(self):
+        self.Login_Account()
 
-merhaba ben ibrahim yilmaz
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    sys.exit(app.exec_())
 
-"""
-
-em = EmailMessage() #this is an instance of EmailMessage
-em['From'] = email_sender
-em['To'] = email_receiver
-em['subject'] = subject
-em.set_content(body)
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-    smtp.login(email_sender, email_password)
-    smtp.sendmail(email_sender, email_receiver, em.as_string())
+if __name__ == '__main__':
+    main()
